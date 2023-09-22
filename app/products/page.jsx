4624180ca287aa8dbabfe4db11460e32aee4940a
thelordzeus <arnav55125@gmail.com>
2023-09-22 16:38:51 +0530

    feat: added sort functionality

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -1,6 +1,32 @@
+"use client";
 import PRODUCTS from "@/data/products";
+import React, { useState } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export default function Products() {
+  const [filteredProducts, setFilteredProducts] = useState(PRODUCTS);
+  const [sortOrder, setSortOrder] = useState("ascending");
+
+  const handleSortAscending = () => {
+    const sorted = [...filteredProducts].sort((a, b) => a.price - b.price);
+    setFilteredProducts(sorted);
+    console.log(sorted);
+    setSortOrder("ascending");
+  };
+
+  const handleSortDescending = () => {
+    const sorted = [...filteredProducts].sort((a, b) => b.price - a.price);
+    setFilteredProducts(sorted);
+    console.log(sorted);
+    setSortOrder("descending");
+  };
   return (
     <div className="bg-white py-6 sm:py-8 lg:py-12">
       <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
@@ -9,16 +35,22 @@ export default function Products() {
             Explore our wide range of collection
           </h2>
 
-          <a
-            href="#"
+          <button
             className="inline-block rounded-lg border bg-white px-4 py-2 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-100 focus-visible:ring active:bg-gray-200 md:px-8 md:py-3 md:text-base"
+            onClick={handleSortAscending}
           >
-            Filter
-          </a>
+            Sort by Price (Low to High)
+          </button>
+          <button
+            className="inline-block rounded-lg border bg-white px-4 py-2 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-100 focus-visible:ring active:bg-gray-200 md:px-8 md:py-3 md:text-base"
+            onClick={handleSortDescending}
+          >
+            Sort by Price (High to Low)
+          </button>
         </div>
 
         <ul className="grid gap-x-4 gap-y-8 sm:grid-cols-2 md:gap-x-6 lg:grid-cols-3 xl:grid-cols-4">
-          {PRODUCTS.map((product) => (
+          {filteredProducts.map((product) => (
             <li key={product.id}>
               <div>
                 <a
