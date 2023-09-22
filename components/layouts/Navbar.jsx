bee5fca4dbd26980ec6628a1d898748f4d89602c
thelordzeus <arnav55125@gmail.com>
2023-09-22 15:14:59 +0530

    fix: navbar responsive issue

--- components/layouts/Navbar.jsx
+++ components/layouts/Navbar.jsx
@@ -1,5 +1,14 @@
 import { PiSneakerBold } from "react-icons/pi";
 import { AiOutlineShoppingCart } from "react-icons/ai";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 export default function Navbar() {
   return (
     <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
@@ -12,23 +21,25 @@ export default function Navbar() {
           <PiSneakerBold className="h-auto w-6" />
           Snicks
         </a>
-        <nav className="hidden gap-12 lg:flex">
-          <a href="#" className="text-lg font-semibold text-indigo-500">
-            Home
-          </a>
-          <a
-            href="#"
-            className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700"
-          >
-            Products
-          </a>
-          <a
-            href="#"
-            className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700"
-          >
-            About
-          </a>
-        </nav>
+        <div className="hidden lg:block">
+          <nav className="  gap-12 lg:flex">
+            <a href="#" className="text-lg font-semibold text-indigo-500">
+              Home
+            </a>
+            <a
+              href="#"
+              className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700"
+            >
+              Products
+            </a>
+            <a
+              href="#"
+              className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700"
+            >
+              About
+            </a>
+          </nav>
+        </div>
         <a
           href="#"
           className="hidden rounded-lg bg-gray-200 px-8 py-3 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base lg:inline-block"
@@ -38,24 +49,17 @@ export default function Navbar() {
             <span>Cart</span>
           </div>
         </a>
-        <button
-          type="button"
-          className="inline-flex items-center gap-2 rounded-lg bg-gray-200 px-2.5 py-2 text-sm font-semibold text-gray-500 ring-indigo-300 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base lg:hidden"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-              clip-rule="evenodd"
-            />
-          </svg>
-          Menu
-        </button>
+        <div className="lg:hidden rounded-lg bg-gray-200 px-8 py-3 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base">
+          <DropdownMenu>
+            <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem>Home</DropdownMenuItem>
+              <DropdownMenuItem>Products</DropdownMenuItem>
+              <DropdownMenuItem>About</DropdownMenuItem>
+              <DropdownMenuItem>Cart</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
       </header>
     </div>
   );
